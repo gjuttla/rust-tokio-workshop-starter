00eee817e2089c3bf70518466e956eb1b8efb18d
Gurpreet Juttla <22775678+gjuttla@users.noreply.github.com>
2021-12-07 14:11:33 +0000

    Update chat impl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use tokio::{net::TcpListener, io::{AsyncWriteExt, AsyncReadExt, BufReader, AsyncBufReadExt}};
+use std::net::SocketAddr;
+
+use tokio::{net::TcpListener, io::{AsyncWriteExt, AsyncReadExt, BufReader, AsyncBufReadExt}, sync::broadcast};
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
     let listener = TcpListener::bind("localhost:8001").await?;
 
+    let (tx, rx) = broadcast::channel::<(SocketAddr, String)>(100);
+
     loop {
         let (mut socket, addr) = listener.accept().await?;
         println!("Listening on {}", addr);
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn( async move {
             loop {
@@ -14,11 +21,21 @@ async fn main() -> tokio::io::Result<()> {
                 let mut reader = BufReader::new(reader);
                 let mut buffer = String::new();
 
-                if reader.read_line(&mut buffer).await.is_err() || buffer.trim() == "exit" {
-                    println!("Disconnected {}", addr);
-                    break;
-                }
-                writer.write_all(buffer.as_bytes()).await.unwrap();
+                tokio::select! {
+                    msg = rx.recv() => {
+                        let (other_addr, msg) = msg.unwrap();
+                        if other_addr != addr {
+                            writer.write_all(format!("{}: {}", other_addr, msg).as_bytes()).await.unwrap();    
+                        }
+                    }
+                    result = reader.read_line(&mut buffer) => {
+                        if result.is_err() || buffer.trim() == "exit" {
+                            println!("Disconnected {}", addr);
+                            break;
+                        }
+                        tx.send((addr, buffer.clone())).unwrap();
+                    }
+                }                
             }
             println!("Connection closed on {}", addr);
         });
