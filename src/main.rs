52efa234701307607c4051be54e69817b651f50e
Gurpreet Juttla <22775678+gjuttla@users.noreply.github.com>
2021-12-07 14:27:10 +0000

    Update chat impl

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tokio::{net::TcpListener, io::{AsyncWriteExt, AsyncReadExt, BufReader, Async
 async fn main() -> tokio::io::Result<()> {
     let listener = TcpListener::bind("localhost:8001").await?;
 
-    let (tx, rx) = broadcast::channel::<(SocketAddr, String)>(100);
+    let (tx, _rx) = broadcast::channel::<(SocketAddr, String)>(100);
 
     loop {
         let (mut socket, addr) = listener.accept().await?;
@@ -16,18 +16,21 @@ async fn main() -> tokio::io::Result<()> {
         let mut rx = tx.subscribe();
 
         tokio::spawn( async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+
             loop {
-                let (reader, mut writer) = socket.split();
-                let mut reader = BufReader::new(reader);
                 let mut buffer = String::new();
 
                 tokio::select! {
+                    // Reads from channel, sends to socket
                     msg = rx.recv() => {
                         let (other_addr, msg) = msg.unwrap();
                         if other_addr != addr {
                             writer.write_all(format!("{}: {}", other_addr, msg).as_bytes()).await.unwrap();    
                         }
                     }
+                    // Reads from socket, sends to channel
                     result = reader.read_line(&mut buffer) => {
                         if result.is_err() || buffer.trim() == "exit" {
                             println!("Disconnected {}", addr);
